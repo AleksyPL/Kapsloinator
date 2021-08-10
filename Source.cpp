0903934723bb750ae297310196c40f1356555d1e
Michał Aleksandrowicz <45513444+AleksyPL@users.noreply.github.com>
2021-08-10 18:31:09 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,4 @@
 #include "opencv2\opencv.hpp"
-//#include <boost/filesystem.hpp>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -10,7 +9,7 @@
 #include <filesystem>
 
 using namespace cv;
-//using namespace boost::filesystem;
+namespace fs = std::filesystem;
 using std::endl;
 using std::cout;
 using std::cin;
@@ -18,55 +17,24 @@ using std::string;
 using std::vector;
 using std::fstream;
 using std::ios;
-namespace fs = std::filesystem;
 
-string extractLastFolderName(string path)
+void generateTextFile(string finalPath, string fileName, vector<string>& vectorFile)
 {
-	string stringToReturn = "";
-	int fileNameBegin=0;
-	for (int i = path.size()-1; i > 0; i--)
+	std::fstream file;
+	file.open(finalPath + "\\" + fileName + ".txt", ios::out);
+	for (int i = 0; i < vectorFile.size(); i++)
 	{
-		if (path[i] == '\\')
+		if (i == vectorFile.size() - 1)
 		{
-			fileNameBegin = i;
-			break;
+			file << vectorFile[i];
 		}
-	}
-	for (int i = fileNameBegin+1; i < path.size(); i++)
-	{
-		stringToReturn += path[i];
-	}
-	return stringToReturn;
-}
-int returnAmountOfFilesInDirectory(string pathString)
-{
-	int i = 0;
-	for (auto& entry : fs::directory_iterator(pathString))
-	{
-		if (entry.is_regular_file())
+		else
 		{
-			i++;
+			file << vectorFile[i] << endl;
 		}
+		
 	}
-	return i;
-	/*boost::filesystem::path the_path(pathString);
-	int cnt = std::count_if(directory_iterator(the_path), directory_iterator(), static_cast<bool(*)(const boost::filesystem::path&)>(is_regular_file));
-	return cnt;*/
-}
-void returnPathsToFoldersInsideTheFolder(string sourcePath, vector<string>&insidePaths)
-{
-	for (auto& entry : fs::directory_iterator(sourcePath))
-	{
-		if (entry.is_regular_file())
-		{
-			insidePaths.push_back(entry.path().string());
-		}
-	}
-	/*boost::filesystem::path the_path(sourcePath);
-	for (auto& p : boost::filesystem::directory_iterator(the_path))
-	{
-		insidePaths.push_back(p.path().string());
-	}*/
+	file.close();
 }
 string getTime()
 {
@@ -108,221 +76,225 @@ string getDate()
 		return { str[8], str[9],'.','1','2','.', str[20],str[21],str[22],str[23] };
 
 }
-void generateTextFile(string finalPath, string fileName, vector<string>& vectorFile)
+string extractLastFolderName(string path)
 {
-	std::fstream file;
-	file.open(finalPath + "\\" + fileName + ".txt", ios::out);
-	for (int i = 0; i < vectorFile.size(); i++)
+	string stringToReturn = "";
+	int fileNameBegin=0;
+	for (int i = path.size()-1; i > 0; i--)
 	{
-		if (i == vectorFile.size() - 1)
+		if (path[i] == '\\')
 		{
-			file << vectorFile[i];
+			fileNameBegin = i;
+			break;
+		}
+	}
+	for (int i = fileNameBegin+1; i < path.size(); i++)
+	{
+		stringToReturn += path[i];
+	}
+	return stringToReturn;
+}
+void returnPaths(string path, vector<fs::path>& insidePaths)
+{
+	for (auto& entry : fs::directory_iterator(path))
+	{
+		if (entry.is_directory())
+		{
+			returnPaths(entry.path().string(), insidePaths);
+		}
+		else if (entry.is_regular_file())
+		{
+			if (entry.path().extension() == ".jpg" || entry.path().extension() == ".png")
+			{
+				insidePaths.push_back(entry.path());
+			}
+		}
+	}
+}
+void dividePathsByCountry(vector<fs::path> &insidePaths, vector<vector<fs::path>> &objects)
+{
+	vector<fs::path> dividedPaths;
+	string lastCountry = "";
+	string extractedCountry = "";
+	for (int i = 0; i < insidePaths.size(); i++)
+	{
+		extractedCountry = extractLastFolderName(insidePaths[i].parent_path().string());
+		if (lastCountry == "")
+		{
+			lastCountry = extractedCountry;
+			dividedPaths.push_back(insidePaths[i]);
+		}
+		else if (lastCountry == extractedCountry)
+		{
+			dividedPaths.push_back(insidePaths[i]);
 		}
 		else
 		{
-			file << vectorFile[i] << endl;
+			objects.push_back(dividedPaths);
+			dividedPaths.clear();
+			lastCountry = extractedCountry;
+			dividedPaths.push_back(insidePaths[i]);
 		}
-		
 	}
-	file.close();
+	if (std::find(objects.begin(), objects.end(), dividedPaths) == objects.end())
+	{
+		objects.push_back(dividedPaths);
+	}
 }
-float processFilesFromDirectory(string path, string finalPath, vector<string>&errorLog, vector<string>&raport)
+float processFiles(vector<string>insidePaths, string fileName, string finalPath, vector<string>& errorLog, vector<string>& raport)
 {
 	std::chrono::duration<float> timeDuration;
 	auto start = std::chrono::high_resolution_clock::now();
 	try
 	{
 		Mat errorImg = imread("./Error/Error.jpg", IMREAD_UNCHANGED);
-		cout << extractLastFolderName(path) << ": Work in progress..." << endl;
-		vector<string>insidePaths;
-		for (auto& entry : fs::directory_iterator(path))
-		{
-			if (entry.path().extension() == ".jpg" || entry.path().extension() == ".png")
-			{
-				insidePaths.push_back(entry.path().string());
-			}
-		}
-		/*boost::filesystem::path the_path(path);
-		for (auto& p : boost::filesystem::directory_iterator(the_path))
-		{
-			if (boost::filesystem::extension(p.path()) == ".jpg" || boost::filesystem::extension(p.path()) == ".png")
-			{
-				insidePaths.push_back(p.path().string());
-			}
-		}*/
-		if (insidePaths.size() == 0)
-		{
-			string message = getDate() + " " + getTime() + " Folder " + extractLastFolderName(path) + " is empty. Skipping folder.";
-			raport.push_back(extractLastFolderName(path) + ": " + "0");
-			errorLog.push_back(message);
-			auto stop = std::chrono::high_resolution_clock::now();
-			timeDuration = stop - start;
-			return timeDuration.count();
-		}
-		else
-		{
-			//int rowsInPhoto = 0;
-			//int colsInPhoto = 0;
-			int targetHeight = 7;
-			int targetWidth = 6;
-			
-			if (insidePaths.size() > (targetHeight * targetWidth))
-			{
-				int picturesPart = 0;
-				int indexX = 0;
-				int indexY = 0;
-				cv::Mat finalImg(targetHeight * 1000, targetWidth * 1000, errorImg.type(), Scalar(0, 0, 0));
-				//files
-				for (int i = 0; i < insidePaths.size(); i++)
-				{
-					Mat inProgressImg = imread(insidePaths[i], IMREAD_UNCHANGED);
-					if (inProgressImg.cols == 0 || inProgressImg.rows == 0)
-					{
-						string message = getDate() + " " + getTime() + " Picture nr. " + std::to_string(i) + " in " + extractLastFolderName(path) + " cannot be loaded. Skipping picture.";
-						errorLog.push_back(message);
-						cout << message << endl;
-						inProgressImg = errorImg;
-					}
-					else if (inProgressImg.cols != 1000 || inProgressImg.rows != 1000)
-					{
-						Size size(1000, 1000);
-						resize(inProgressImg, inProgressImg, size);
-					}
-					//rows
-					for (int j = 0; j < 1000; j++)
-					{
-						//cols
-						for (int k = 0; k < 1000; k++)
-						{
-							//3 channels
-							for (int l = 0; l < 3; l++)
-							{
-								finalImg.at<Vec3b>(indexY * 1000 + j, indexX * 1000 + k)[l] = inProgressImg.at<Vec3b>(j, k)[l];
-							}
-						}
-					}
-					indexX++;
-					if (indexX == targetWidth)
-					{
-						if (indexY == targetHeight - 1)
-						{
-							cv::imwrite(finalPath + "/" + extractLastFolderName(path) + " - FINAL(" + std::to_string(picturesPart) + ").jpg", finalImg);
-							int remainingFiles = insidePaths.size() - i - 1;
-							if (remainingFiles > targetHeight * targetWidth)
-							{
-								finalImg = Mat::zeros(targetHeight * 1000, targetWidth * 1000, finalImg.type());
-							}
-							else
-							{
-								int newSizeX = 0;
-								int newSizeY = 0;
-								//setting Y size
-								if (remainingFiles % targetHeight != 0)
-								{
-									newSizeY = remainingFiles / targetHeight + 1;
-								}
-								else
-								{
-									newSizeY = remainingFiles / targetHeight;
-								}
-								//setting X size
-								if (remainingFiles < targetWidth)
-								{
-									newSizeX = remainingFiles;
-								}
-								else
-								{
-									newSizeX = targetWidth;
-								}
-								finalImg = Mat::zeros(newSizeY * 1000, newSizeX * 1000, finalImg.type());
-							}
-							picturesPart++;
-							indexX = 0;
-							indexY = 0;
-						}
-						else
-						{
-							indexX = 0;
-							indexY++;
-						}
-					}
-				}
-				cv::imwrite(finalPath + "/" + extractLastFolderName(path) + " - FINAL(" + std::to_string(picturesPart) + ").jpg", finalImg);
-				auto stop = std::chrono::high_resolution_clock::now();
-				timeDuration = stop - start;
-				cout << "Folder " + extractLastFolderName(path) + " processed sucessfully in " << timeDuration.count() << " s." << endl;
-			}
-			else
-			{
-				int indexX = 0;
-				int indexY = 0;
-				int pictureSizeX = 0;
-				int pictureSizeY = 0;
-				//setting Y size
-				if (insidePaths.size() % targetWidth != 0)
-				{
-					pictureSizeY = insidePaths.size() / targetWidth + 1;
-				}
-				else
-				{
-					pictureSizeY = insidePaths.size() / targetWidth;
-				}
-				//setting X size
-				if (insidePaths.size() < targetWidth)
-				{
-					pictureSizeX = insidePaths.size();
-				}
-				else
-				{
-					pictureSizeX = targetWidth;
-				}
-				cv::Mat finalImg(pictureSizeY * 1000, pictureSizeX * 1000, errorImg.type(), Scalar(0, 0, 0));
-				//files
-				for (int i = 0; i < insidePaths.size(); i++)
-				{
-					Mat inProgressImg = imread(insidePaths[i], IMREAD_UNCHANGED);
-					if (inProgressImg.cols == 0 || inProgressImg.rows == 0)
-					{
-						string message = getDate() + " " + getTime() + " Picture nr. " + std::to_string(i) + " in " + extractLastFolderName(path) + " cannot be loaded. Skipping picture.";
-						errorLog.push_back(message);
-						cout << message << endl;
-						inProgressImg = errorImg;
-					}
-					else if (inProgressImg.cols != 1000 || inProgressImg.rows != 1000)
-					{
-						Size size(1000, 1000);
-						resize(inProgressImg, inProgressImg, size);
-					}
-					//rows
-					for (int j = 0; j < 1000; j++)
-					{
-						//cols
-						for (int k = 0; k < 1000; k++)
-						{
-							//3 channels
-							for (int l = 0; l < 3; l++)
-							{
-								finalImg.at<Vec3b>(indexY * 1000 + j, indexX * 1000 + k)[l] = inProgressImg.at<Vec3b>(j, k)[l];
-							}
-						}
-					}
-					indexX++;
-					if (indexX == targetWidth)
-					{
-						indexX = 0;
-						indexY++;
-					}
-				}
-				cv::imwrite(finalPath + "/" + extractLastFolderName(path) + " - FINAL.jpg", finalImg);
-				auto stop = std::chrono::high_resolution_clock::now();
-				timeDuration = stop - start;
-				cout << "Folder " + extractLastFolderName(path) + " processed sucessfully in " << timeDuration.count() << " s." << endl;
-			}
-		}
+		cout << fileName << ": Work in progress..." << endl;
+		int targetHeight = 7;
+		int targetWidth = 6;
+		//if (insidePaths.size() > (targetHeight * targetWidth))
+		//{
+		//	int picturesPart = 0;
+		//	int indexX = 0;
+		//	int indexY = 0;
+		//	cv::Mat finalImg(targetHeight * 1000, targetWidth * 1000, errorImg.type(), Scalar(0, 0, 0));
+		//	//files
+		//	for (int i = 0; i < insidePaths.size(); i++)
+		//	{
+		//		Mat inProgressImg = imread(insidePaths[i], IMREAD_UNCHANGED);
+		//		if (inProgressImg.cols != 1000 || inProgressImg.rows != 1000)
+		//		{
+		//			Size size(1000, 1000);
+		//			resize(inProgressImg, inProgressImg, size);
+		//		}
+		//		//rows
+		//		for (int j = 0; j < 1000; j++)
+		//		{
+		//			//cols
+		//			for (int k = 0; k < 1000; k++)
+		//			{
+		//				//3 channels
+		//				for (int l = 0; l < 3; l++)
+		//				{
+		//					finalImg.at<Vec3b>(indexY * 1000 + j, indexX * 1000 + k)[l] = inProgressImg.at<Vec3b>(j, k)[l];
+		//				}
+		//			}
+		//		}
+		//		indexX++;
+		//		if (indexX == targetWidth)
+		//		{
+		//			if (indexY == targetHeight - 1)
+		//			{
+		//				cv::imwrite(finalPath + "/" + fileName + " - FINAL(" + std::to_string(picturesPart) + ").jpg", finalImg);
+		//				int remainingFiles = insidePaths.size() - i - 1;
+		//				if (remainingFiles > targetHeight * targetWidth)
+		//				{
+		//					finalImg = Mat::zeros(targetHeight * 1000, targetWidth * 1000, finalImg.type());
+		//				}
+		//				else
+		//				{
+		//					int newSizeX = 0;
+		//					int newSizeY = 0;
+		//					//setting Y size
+		//					if (remainingFiles % targetHeight != 0)
+		//					{
+		//						newSizeY = remainingFiles / targetHeight + 1;
+		//					}
+		//					else
+		//					{
+		//						newSizeY = remainingFiles / targetHeight;
+		//					}
+		//					//setting X size
+		//					if (remainingFiles < targetWidth)
+		//					{
+		//						newSizeX = remainingFiles;
+		//					}
+		//					else
+		//					{
+		//						newSizeX = targetWidth;
+		//					}
+		//					finalImg = Mat::zeros(newSizeY * 1000, newSizeX * 1000, finalImg.type());
+		//				}
+		//				picturesPart++;
+		//				indexX = 0;
+		//				indexY = 0;
+		//			}
+		//			else
+		//			{
+		//				indexX = 0;
+		//				indexY++;
+		//			}
+		//		}
+		//	}
+		//	cv::imwrite(finalPath + "/" + fileName + " - FINAL(" + std::to_string(picturesPart) + ").jpg", finalImg);
+		//	auto stop = std::chrono::high_resolution_clock::now();
+		//	timeDuration = stop - start;
+		//	cout << "Folder " + fileName + " processed sucessfully in " << timeDuration.count() << " s." << endl;
+		//	raport.push_back(fileName + ": " + std::to_string(insidePaths.size()));
+		//}
+		//else
+		//{
+		//	int indexX = 0;
+		//	int indexY = 0;
+		//	int pictureSizeX = 0;
+		//	int pictureSizeY = 0;
+		//	//setting Y size
+		//	if (insidePaths.size() % targetWidth != 0)
+		//	{
+		//		pictureSizeY = insidePaths.size() / targetWidth + 1;
+		//	}
+		//	else
+		//	{
+		//		pictureSizeY = insidePaths.size() / targetWidth;
+		//	}
+		//	//setting X size
+		//	if (insidePaths.size() < targetWidth)
+		//	{
+		//		pictureSizeX = insidePaths.size();
+		//	}
+		//	else
+		//	{
+		//		pictureSizeX = targetWidth;
+		//	}
+		//	cv::Mat finalImg(pictureSizeY * 1000, pictureSizeX * 1000, errorImg.type(), Scalar(0, 0, 0));
+		//	//files
+		//	for (int i = 0; i < insidePaths.size(); i++)
+		//	{
+		//		Mat inProgressImg = imread(insidePaths[i], IMREAD_UNCHANGED);
+		//		if (inProgressImg.cols != 1000 || inProgressImg.rows != 1000)
+		//		{
+		//			Size size(1000, 1000);
+		//			resize(inProgressImg, inProgressImg, size);
+		//		}
+		//		//rows
+		//		for (int j = 0; j < 1000; j++)
+		//		{
+		//			//cols
+		//			for (int k = 0; k < 1000; k++)
+		//			{
+		//				//3 channels
+		//				for (int l = 0; l < 3; l++)
+		//				{
+		//					finalImg.at<Vec3b>(indexY * 1000 + j, indexX * 1000 + k)[l] = inProgressImg.at<Vec3b>(j, k)[l];
+		//				}
+		//			}
+		//		}
+		//		indexX++;
+		//		if (indexX == targetWidth)
+		//		{
+		//			indexX = 0;
+		//			indexY++;
+		//		}
+		//	}
+		//	cv::imwrite(finalPath + "/" + fileName + " - FINAL.jpg", finalImg);
+		//	auto stop = std::chrono::high_resolution_clock::now();
+		//	timeDuration = stop - start;
+		//	cout << "Folder " + fileName + " processed sucessfully in " << timeDuration.count() << " s." << endl;
+		//	raport.push_back(fileName + ": " + std::to_string(insidePaths.size()));
+		//}
 	}
 	catch (cv::Exception& e)
 	{
-		string message = getDate() + " " + getTime() + " - Error occured in " + extractLastFolderName(path) + " folder. Skipping folder.";
+		string message = getDate() + " " + getTime() + " - Error occured in " + fileName + " folder. Skipping folder.";
 		errorLog.push_back(message);
 		cout << message << endl;
 		auto stop = std::chrono::high_resolution_clock::now();
@@ -331,45 +303,20 @@ float processFilesFromDirectory(string path, string finalPath, vector<string>&er
 	}
 	return timeDuration.count();
 }
-//void checkPathForSlashes(string& path)
-//{
-//	int stringSize = path.length();
-//	for (int i = 0; i < stringSize; i++)
-//	{
-//		if ((i - 1) >= 0 && (i + 1) <= stringSize && path[i] == '\\' && path[i - 1] != '\\' && path[i + 1] != '\\')
-//		{
-//			string tempBeforeSlash= "";
-//			string tempAfterSlash = "";
-//			for (int j = 0; j < i; j++)
-//			{
-//				tempBeforeSlash += path[i];
-//			}
-//			for (int j = i; j < stringSize; j++)
-//			{
-//				tempAfterSlash += path[i];
-//			}
-//			path = tempBeforeSlash + '\\' + tempAfterSlash;
-//			stringSize++;
-//		}
-//	}
-//}
 int main()
 {
 	cout << "Kapsloinator by AleksyPL" << endl;
-	cout << "Path to source folder: ";
-	string path = "E:\\Zdjêcia\\Kaplse\\Raw\\Egipt";
-	returnAmountOfFilesInDirectory(path);
+	//cout << "Path to source folder: ";
+	string startPath = "E:\\Zdjêcia\\Kaplse\\Raw";
 	//std::getline(cin, path);
-	//checkPathForSlashes(path);
-	cout << "Path to final folder: ";
+	//cout << "Path to final folder: ";
 	string finalPath = "E:\\Zdjêcia\\Kaplse\\Final";
 	//std::getline(cin, finalPath);
-	int filesAmount;
-	float totalTime = 0;
-	int totalFiles = 0;
 	vector<string>errorLog;
 	vector<string>raport;
-	if (path.size() == 0 || path.find('\\') == std::string::npos || finalPath.size() == 0 || finalPath.find('\\') == std::string::npos)
+	float totalTime = 0;
+	int totalFiles = 0;
+	if (startPath.size() == 0 || startPath.find('\\') == std::string::npos || finalPath.size() == 0 || finalPath.find('\\') == std::string::npos)
 	{
 		string message = getDate() + " " + getTime() + " Paths may be corrupted";
 		errorLog.push_back(message);
@@ -377,54 +324,24 @@ int main()
 	}
 	else
 	{
-		filesAmount = returnAmountOfFilesInDirectory(path);
-		if (filesAmount == 0)
+		vector<fs::path> rawPaths;
+		vector<vector<fs::path>>objects;
+		returnPaths(startPath, rawPaths);
+		dividePathsByCountry(rawPaths, objects);
+		for (int i = 0; i < objects.size(); i++)
 		{
-			//all folders
-			vector<string>pathsToFoldersInside;
-			returnPathsToFoldersInsideTheFolder(path, pathsToFoldersInside);
-			for (int i = 0; i < pathsToFoldersInside.size(); i++)
+			vector <string> insidePaths;
+			for (int j = 0; j < objects[i].size(); j++)
 			{
-				if (returnAmountOfFilesInDirectory(pathsToFoldersInside[i]) == 0)
-				{
-					string message = getDate() + " " + getTime() + " Folder " + extractLastFolderName(pathsToFoldersInside[i]) + " is empty. Skipping folder.";
-					raport.push_back(extractLastFolderName(pathsToFoldersInside[i]) + ": " + "0");
-					errorLog.push_back(message);
-					cout << message << endl;
-					pathsToFoldersInside.erase(pathsToFoldersInside.begin() + i);
-				}
+				insidePaths.push_back(objects[i][j].string());
 			}
-			if (pathsToFoldersInside.size() == 0)
-			{
-				string message = getDate() + " " + getTime() + " Folder does not contain any data to process.";
-				errorLog.push_back(message);
-				cout << message << endl;
-			}
-			else
-			{
-				for (int i = 0; i < pathsToFoldersInside.size(); i++)
-				{
-					raport.push_back(extractLastFolderName(pathsToFoldersInside[i]) + ": " + std::to_string(returnAmountOfFilesInDirectory(pathsToFoldersInside[i])));
-					totalTime += processFilesFromDirectory(pathsToFoldersInside[i],finalPath, errorLog, raport);
-					totalFiles += returnAmountOfFilesInDirectory(pathsToFoldersInside[i]);
-				}
-				raport.push_back("");
-				raport.push_back("Total: " + std::to_string(totalFiles));
-				cout << "DONE!" << endl;
-				cout << "Total time: " << totalTime << " s." << endl;
-			}
+			totalTime += processFiles(insidePaths, extractLastFolderName(objects[i][0].parent_path().string()), finalPath, errorLog, raport);
+			totalFiles += insidePaths.size();
 		}
-		else
-		{
-			//only one folder
-			raport.push_back(extractLastFolderName(path) + ": " + std::to_string(returnAmountOfFilesInDirectory(path)));
-			totalFiles += returnAmountOfFilesInDirectory(path);
-			raport.push_back("");
-			raport.push_back("Total: " + std::to_string(totalFiles));
-			totalTime += processFilesFromDirectory(path, finalPath, errorLog, raport);
-			cout << "DONE!" << endl;
-			cout << "Total time: " << totalTime << " s." << endl;
-		}
+		raport.push_back("");
+		raport.push_back("Total: " + std::to_string(totalFiles));
+		cout << "DONE!" << endl;
+		cout << "Total time: " << totalTime << " s." << endl;
 	}
 	generateTextFile(finalPath, "SummaryReport", raport);
 	if (errorLog.size() != 0)
